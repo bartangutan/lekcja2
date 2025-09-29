f03f19d6ecf87db46db4f2a0b4f52445714cd25b
bartangutan <19bartosz11@gmail.com>
2025-09-29 14:18:51 +0200

    losowe liczby

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //10 liczb z zakresu 1-20
+        Random random = new Random();
+        int[] losoweLiczby = new int[10];
+        for(int i=0; i<losoweLiczby.length; i++){
+            losoweLiczby[i]= random.nextInt(20)+1;
+        }
+
+        System.out.println("Wszystkie liczby wylosowane: ");
+        for (int elementTablicy:losoweLiczby) {
+            System.out.println(elementTablicy+", ");
+        }
+        //System.out.println();
     }
+
 }
